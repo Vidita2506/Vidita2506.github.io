d85d175700fc7a7ab763c6769758e1969a8c0713
vidita <vidita@Viditas-MacBook-Pro.local>
2020-09-04 15:39:18 -0700

    Updates

---
+++ precache-manifest.d01c0e38146257a94611397f18bc8449.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8a2158eb8046fe08c23514a742a68c7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c7ac8acb40ffb8cb6026",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "b443032f112967740f52",
+    "url": "/static/css/main.f60eae6a.chunk.css"
+  },
+  {
+    "revision": "c7ac8acb40ffb8cb6026",
+    "url": "/static/js/2.0789156f.chunk.js"
+  },
+  {
+    "revision": "b25e0c44d0b24745f7d62e2ea6be31e5",
+    "url": "/static/js/2.0789156f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b443032f112967740f52",
+    "url": "/static/js/main.d1e50b5d.chunk.js"
+  },
+  {
+    "revision": "efe25b4b8d1f7af9da4b",
+    "url": "/static/js/runtime-main.6b3488d1.js"
+  }
+]);
